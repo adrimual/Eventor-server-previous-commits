970702d3282b18a554266e59b7b970cc5586b255
adrimual <adrimual14@gmail.com>
2020-08-21 13:38:02 +0200

    finishing profile and edit profile

--- routes/user/profile/index.js
+++ routes/user/profile/index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10
 //Models
-
-const User = require('../../../models/user.model')
+const User = require('../../../models/user.model');
+const Person = require('../../../models/person.model');
 
 //Endpoints to edit username and password
 
 router.post('/edit/:id', (req, res, next) => {
-    const { username, email, password, avatar, age, genre, vehicle } = req.body;
+    const { username, email, password} = req.body;
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
 
@@ -17,17 +19,17 @@ router.post('/edit/:id', (req, res, next) => {
         })
         return
     }
-    User
-        .findByIdAndUpdate(req.params.id, {
-            username,
-            email,
-            password: hashPass,
-            avatar,
-            age,
-            genre,
-            vehicle 
-        })
-        .then((response) => res.json(response))
+        User
+            .findById(req.params.id)
+            .populate('personDetails')
+            .then((user) => {
+                user.username = username
+                user.email = email
+                user.password = hashPass
+                user.save()
+                return user.personDetails
+            })
+        // .then((response) => res.json(response))
         .catch(err => next(new Error(err)))
 })
 module.exports = router
