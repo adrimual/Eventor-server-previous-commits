04086efb5049b34746f34b163b5bb9053b31ff27
adrimual <adrimual14@gmail.com>
2020-08-25 09:57:23 +0200

    adding details of events in profile

--- routes/user/event/index.js
+++ routes/user/event/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
+//Models
+const User = require('../../../models/user.model')
+const Person = require('../../../models/person.model')
 const Event = require('../../../models/event.model')
+//Create an Event
 router.post('/create', (req, res, next) => {
     Event
         .create(req.body)
+        .then(response => console.log(response.data))
+        .catch(err => next(err))
+})
+router.get('/:person_id', (req, res) => {
+    Event
+        .find( {owner: req.params.person_id})
         .then(response => res.json(response))
         .catch(err => next(err))
 })
