e490254bd74108ba9b19612fa9890722bf150d51
adrimual <adrimual14@gmail.com>
2020-08-26 12:00:14 +0200

    preparing for deploy

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ module.exports = app => {
         app.use('/', require('./base'))
         app.use('/api', require('./auth'))
         app.use('/api/user', require('./user'))
+        app.use((req, res) => {
+        res.sendFile(__dirname + "/public/index.html");
+  })
 }
