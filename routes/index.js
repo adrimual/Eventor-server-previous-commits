0def77f38bb70220e33769685b5b485b31b26669
adrimual <adrimual14@gmail.com>
2020-08-26 14:06:00 +0200

    fixing index route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ module.exports = app => {
         app.use('/', require('./base'))
         app.use('/api', require('./auth'))
         app.use('/api/user', require('./user'))
-        app.use('/api/files', require('./files'))
         app.use((req, res) => {
           res.sendFile(__dirname + "/public/index.html");
         })
